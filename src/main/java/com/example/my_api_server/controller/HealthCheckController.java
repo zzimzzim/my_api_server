829487560183937dc2ebdfd1e71683367d188e5e
zzimzzim <kju1354@naver.com>
2026-04-09 15:21:30 +0900

    배포테스트 2

--- src/main/java/com/example/my_api_server/controller/HealthCheckController.java
+++ src/main/java/com/example/my_api_server/controller/HealthCheckController.java
@@ -13,7 +13,7 @@ public class HealthCheckController {
 
     @GetMapping
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("연결 성공1");
+        return ResponseEntity.ok("연결 성공2");
     }
 
 }
